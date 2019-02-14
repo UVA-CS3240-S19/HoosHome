044c3c442679c6b6407a7a91d7c596688d3176ab
furhankhan12 <fsk2pd@virginia.edu>
2019-02-14 14:15:17 -0500

    new1

--- hooshome/home/views.py
+++ hooshome/home/views.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-from django.shortcuts import render,redirect
-def home(request):
-    return render(request, "home.html",{})
-
-def gitlink(request):
-    link = redirect('https://github.com/UVA-CS3240-S19/project-102-nautilus')
-    return link
-=======
 from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html",{})
->>>>>>> e4fdc89900070a4344d20db6921cd431d27a4717

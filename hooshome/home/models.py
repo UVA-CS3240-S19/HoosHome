3056ce55453cdc90c1034ea4fb496423b81b2017
Quang Nguyen <qdn8mj@virginia.edu>
2019-02-17 01:36:07 -0500

    Admin, Homepage, and DB table updates

--- hooshome/home/models.py
+++ hooshome/home/models.py
@@ -1,3 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Listing(models.Model):
+    pub_date = models.DateTimeField('date published')
+    address = models.CharField(max_length=200)
+    realtor_agent = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.address
+
+    def published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=50) <= self.pub_date <= now
+
+class Users(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)

1e16a069bb4c1aa51434dbd9b324dad29a4e4a8d
whisiro25 <whisiro@gmail.com>
2019-02-17 01:04:00 -0500

    Added login functionality

--- hooshome/home/views.py
+++ hooshome/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 
 def home(request):
     return render(request, "home.html",{})
@@ -6,3 +8,17 @@ def home(request):
 def gitlink(request):
     link = redirect('https://github.com/UVA-CS3240-S19/project-102-nautilus')
     return link
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})

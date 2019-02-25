782bcc906ba71dc6f9bb0eb59a3cdcd0d7839101
Quang Nguyen <qdn8mj@virginia.edu>
2019-02-25 17:39:37 -0500

    initial login tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+import unittest
+
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Users
+
+class LoginTest(TestCase):
+    def setUp(self):
+        Users.objects.create(username='cavman', password='uva123')
+
+    def test_animals_can_speak(self):
+        """User can be created"""
+        cavman = Users.objects.get(username='cavman')
+        self.assertEqual(cavman.username, 'cavman')
+
+
+
+
+
+

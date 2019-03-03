61bc798d47c56f0e33465640a2a5e07b9484b3d6
Jeffrey Gerken <jeffrey@Jeffreys-MacBook-Pro.local>
2019-03-02 21:54:06 -0500

    Created new requirements-build.txt without heroku dependency for Travis integration

--- hooshome/settings.py
+++ hooshome/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

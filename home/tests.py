fc8cf21ba09a8dd98655e12a31e3d4b13a8c383f
Jeffrey Gerken <jeffrey@Jeffreys-MacBook-Pro.local>
2019-03-02 20:16:12 -0500

    Added listing unit tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Listing
+
+from django.utils import timezone
+class ListingTestClass(TestCase):
+    def testOldListing(self):
+        time = timezone.now() - datetime.timedelta(days=50, seconds=1)
+        old_question = Listing(pub_date=time)
+        self.assertIs(old_question.published_recently(), False)
+
+    def testNewListing(self):
+        time = timezone.now() - datetime.timedelta(days=49)
+        recent_question = Listing(pub_date=time)
+        self.assertIs(recent_question.published_recently(), True)

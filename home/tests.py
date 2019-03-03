08fedec85d30f560ac6011ee2374d387a861d2e1
Jeffrey Gerken <jeffrey@Jeffreys-MacBook-Pro.local>
2019-03-02 20:28:59 -0500

    Added unit tests

--- home/tests.py
+++ home/tests.py
@@ -7,16 +7,26 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import Users
-
+from .models import Users, Listing
 class LoginTest(TestCase):
-    def setUp(self):
-        return Users.objects.create(username='cavman', password='uva123')
-
-    def test_new_user(self):
-        """User can be created"""
-        cavman = Users.objects.get(username='cavman')
+    def testUserPass(self):
+        cavman = Users(username='cavman',password='uva12345')
         self.assertEqual(cavman.username, 'cavman')
+class ListingTest(TestCase):
+    def testFutureListing(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Listing(pub_date=time)
+        self.assertIs(future_question.published_recently(), False)
+
+    def testOldQuestion(self):
+        time = timezone.now() - datetime.timedelta(days=51)
+        old_question = Listing(pub_date=time)
+        self.assertIs(old_question.published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=49)
+        recent_question = Listing(pub_date=time)
+        self.assertIs(recent_question.published_recently(), True)
 
 
 

dbcb374698f5a2f02832f1b692680bba69208859
Jeffrey Gerken <jeffrey@Jeffreys-MacBook-Pro.local>
2019-03-02 23:00:33 -0500

    Changed some test names

--- home/tests.py
+++ home/tests.py
@@ -26,11 +26,11 @@ class ListingTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Listing(pub_date=time)
         self.assertIs(future_question.published_recently(), False)
-    def testOldQuestion(self):
+    def testOldListing(self):
         time = timezone.now() - datetime.timedelta(days=51)
         old_question = Listing(pub_date=time)
         self.assertIs(old_question.published_recently(), False)
-    def testRecentQuestion(self):
+    def testRecentListing(self):
         time = timezone.now() - datetime.timedelta(hours=49)
         recent_question = Listing(pub_date=time)
         self.assertIs(recent_question.published_recently(), True)

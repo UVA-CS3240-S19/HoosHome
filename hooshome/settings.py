7c0c7fd4e4c48c1946cca59042e7a9e9d51e1cb9
Tanner Dellett-Wion <tdellettwion@gmail.com>
2019-03-04 18:05:39 -0500

    switching the ordering of the backends

--- hooshome/settings.py
+++ hooshome/settings.py
@@ -109,8 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
     'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
 )
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/

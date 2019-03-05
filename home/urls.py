9c9f9854c148117fd18207dd0fdd7355296be29b
Neal <hc4pa@virginia.edu>
2019-03-04 19:42:27 -0500

    fixed urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-<<<<<<< HEAD
+
 from django.contrib import admin
 from home import views
 
@@ -7,8 +7,7 @@ urlpatterns = [
     path('', views.home, name="homepage"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('social_django.urls', namespace='social')),
-=======
-<<<<<<< HEAD
+]
 
 from . import views
 
@@ -17,14 +16,4 @@ urlpatterns = [
 	path('createlisting/', views.ListingCreateView.as_view(), name='createlisting'),
     path('listings', views.ListingList.as_view(), name='listings'),
 	path('search', views.search, name='search'),
-=======
-from django.contrib import admin
-from home import views
-
-urlpatterns = [
-    path('', views.home, name="homepage"),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('social_django.urls', namespace='social')),
->>>>>>> master
->>>>>>> listings
 ]

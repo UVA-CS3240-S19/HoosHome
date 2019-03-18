2f73a79f8af1ce3f65bc238bf484e98b700052cd
Quang Nguyen <qdn8mj@virginia.edu>
2019-03-18 17:21:56 -0400

    collision fixes in tests.py

--- home/tests.py
+++ home/tests.py
@@ -1,24 +1,3 @@
-import datetime
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-from django.test import TestCase
-from .models import Listing
-
-from django.utils import timezone
-class ListingTestClass(TestCase):
-    def testOldListing(self):
-        time = timezone.now() - datetime.timedelta(days=50, seconds=1)
-        old_question = Listing(pub_date=time)
-        self.assertIs(old_question.published_recently(), False)
-
-    def testNewListing(self):
-        time = timezone.now() - datetime.timedelta(days=49)
-        recent_question = Listing(pub_date=time)
-        self.assertIs(recent_question.published_recently(), True)
-=======
->>>>>>> listings
 import unittest
 
 from django.test import TestCase

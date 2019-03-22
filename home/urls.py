4cedd054cc548307b27797d0b9513f8996c7161f
whisiro25 <whisiro@gmail.com>
2019-03-21 23:24:22 -0400

    updated links and other stuff

--- home/urls.py
+++ home/urls.py
@@ -1,18 +1,12 @@
 from django.urls import path, include
 
 from django.contrib import admin
-from home import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name="homepage"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('social_django.urls', namespace='social')),
-]
-
-from . import views
-
-urlpatterns = [
-	path('', views.home, name = "homepage"),
 	path('createlisting/', views.ListingCreateView.as_view(), name='createlisting'),
     path('listings', views.ListingList.as_view(), name='listings'),
     path('filters', views.ListingListFilter.as_view(), name='filters'),

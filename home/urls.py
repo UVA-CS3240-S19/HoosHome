ad83b827651a793a0c758812b95282f47a52d8b4
wsh5wb <31604151+wsh5wb@users.noreply.github.com>
2019-04-01 19:18:52 -0400

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.home, name="homepage"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('social_django.urls', namespace='social')),
-    path('createlisting/', views.ListingCreateView.as_view(), name='createlisting'),
+    path('createlisting/', views.ListingCreateView, name='createlisting'),
     path('listings', views.ListingList.as_view(), name='listings'),
     path('filters', views.ListingListFilter.as_view(), name='filters'),
     path('search', views.search, name='search'),

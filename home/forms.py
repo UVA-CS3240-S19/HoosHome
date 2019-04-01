ae99521eaa44dcfa8981c52bccee20ea3adae9b0
wsh5wb <31604151+wsh5wb@users.noreply.github.com>
2019-04-01 19:13:01 -0400

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import Listing
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')

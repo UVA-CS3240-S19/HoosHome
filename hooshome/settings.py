759abda9292e2050893cf59ea69d8fb6935994e7
wsh5wb <31604151+wsh5wb@users.noreply.github.com>
2019-04-01 19:17:42 -0400

    Update settings.py

--- hooshome/settings.py
+++ hooshome/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Google login url
 LOGIN_URL = '/auth/login/google-oauth2'

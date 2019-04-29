fee72797bf0493abcb8e5b32f412473c9beb504a
Tanner Dellett-Wion <tdellettwion@gmail.com>
2019-04-29 10:20:41 -0400

    removed other login

--- home/forms.py
+++ home/forms.py
@@ -4,44 +4,49 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 from .models import Listing
 
-class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
 
-    #class Meta:
-     #   model = User
-      #  fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(
+        max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(
+        max_length=254, help_text='Required. Enter a valid email address.')
+
 
 class SearchForm(forms.Form):
-    search_text = forms.CharField(max_length = 30, required = True)
-    BEDS = (("1","all beds"),("2","1 bed"),("3","2 beds"),("4","3 beds"),("5","3+ beds"))
-    BATHS = (("1","all baths"),("2","1 bath"),("3","2 baths"),("4","3 baths"),("5","3+ baths"))
+    search_text = forms.CharField(max_length=30, required=True)
+    BEDS = (("1", "all beds"), ("2", "1 bed"),
+            ("3", "2 beds"), ("4", "3 beds"), ("5", "3+ beds"))
+    BATHS = (("1", "all baths"), ("2", "1 bath"),
+             ("3", "2 baths"), ("4", "3 baths"), ("5", "3+ baths"))
     beds = forms.ChoiceField(choices=BEDS)
     baths = forms.ChoiceField(choices=BATHS)
     days = forms.ChoiceField(choices=[(x, x) for x in range(1, 5)])
 
+
 class ListingForm(ModelForm):
-    gym = forms.BooleanField(required = False)
-    parking = forms.BooleanField(required = False)
-    wifi = forms.BooleanField(required = False)
-    heating = forms.BooleanField(required = False)
-    furnished = forms.BooleanField(required = False)
-    lounge = forms.BooleanField(required = False)
-    laundry = forms.BooleanField(required = False)
-    pets = forms.BooleanField(required = False)
-    AC = forms.BooleanField(required = False)
-    business_center = forms.BooleanField(required = False)
+    gym = forms.BooleanField(required=False)
+    parking = forms.BooleanField(required=False)
+    wifi = forms.BooleanField(required=False)
+    heating = forms.BooleanField(required=False)
+    furnished = forms.BooleanField(required=False)
+    lounge = forms.BooleanField(required=False)
+    laundry = forms.BooleanField(required=False)
+    pets = forms.BooleanField(required=False)
+    AC = forms.BooleanField(required=False)
+    business_center = forms.BooleanField(required=False)
 
-    baths_list =['1 bath','2 baths', '3 baths', '4+']
+    baths_list = ['1 bath', '2 baths', '3 baths', '4+']
 
-    temp = [(x,x) for x in range(1,5)]
+    temp = [(x, x) for x in range(1, 5)]
 
     baths = forms.ChoiceField(choices=temp)
 
-    beds_list =['1 bed','2 beds', '3 beds', '4+']
+    beds_list = ['1 bed', '2 beds', '3 beds', '4+']
     beds = forms.ChoiceField(choices=temp)
 
     class Meta:
         model = Listing
-        fields = ('address', 'realtor_agent', 'description','front_View','interior_View','back_View', 'price', 'phone_number', 'realtor_site')
+        fields = ('address', 'realtor_agent', 'description', 'front_View',
+                  'interior_View', 'back_View', 'price', 'phone_number', 'realtor_site')
--- home/models.py
+++ home/models.py
@@ -4,33 +4,32 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.utils import timezone
 
+
 class Listing(models.Model):
 
-    #city = models.CharField(max_length=50,default="Charlottesville")
-    #zip = models.CharField("ZIP / Postal code", max_length=12,default=22903)
-    #bedsbaths = models.CharField(max_length=200, default="2B/2B")
-
-    #Listing info
+    # Listing info
     description = models.TextField()
     pub_date = models.DateTimeField('date published', default=timezone.now)
     address = models.CharField(max_length=200)
     price = models.IntegerField()
-    features =  models.CharField(max_length=50, default="[]")
+    features = models.CharField(max_length=50, default="[]")
     beds = models.IntegerField(default=0)
     baths = models.IntegerField(default=0)
-    front_View = models.ImageField(upload_to='media', default="media/vsabre.jpg")
-    interior_View = models.ImageField('Interior View', upload_to='media', default='media/vsabre.jpg')
-    back_View = models.ImageField('Back View', upload_to='media', default='media/vsabre.jpg')
+    front_View = models.ImageField(
+        upload_to='media', default="media/vsabre.jpg")
+    interior_View = models.ImageField(
+        'Interior View', upload_to='media', default='media/vsabre.jpg')
+    back_View = models.ImageField(
+        'Back View', upload_to='media', default='media/vsabre.jpg')
 
-    #Use Json to store lists as strings
+    # Use Json to store lists as strings
     def set_features(self, x):
         self.features = json.dumps(x)
 
     def get_features(self):
         return json.loads(self.features)
 
-
-    #Review info
+    # Review info
     ratings = models.IntegerField(default=0)
     number_of_ratings = models.IntegerField(default=0)
     total_ratings = models.IntegerField(default=0)
@@ -54,13 +53,12 @@ class Listing(models.Model):
         reviewers = self.get_reviewer()
         reviews = self.get_review()
         temp = []
-        for i in range(min(len(reviews),len(reviews))):
-            temp.append((reviews[i],reviewers[i]))
+        for i in range(min(len(reviews), len(reviews))):
+            temp.append((reviews[i], reviewers[i]))
 
         return temp
 
-
-    #Landlord info
+    # Landlord info
     realtor_agent = models.CharField(max_length=200)
     realtor_site = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=15)
@@ -73,13 +71,13 @@ class Listing(models.Model):
         return text[:200] + "......"
 
     def __str__(self):
-        return str(self.address)  + " for " + str(self.price) + "\nDescription: " + str(self.description)
+        return str(self.address) + " for " + str(self.price) + "\nDescription: " + str(self.description)
 
     def published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=50) <= self.pub_date <= now
 
+
 class Users(models.Model):
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
-    

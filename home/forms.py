936088d7525d377324b986c1e7f4f58f7dae04ff
Quang Nguyen <qdn8mj@virginia.edu>
2019-04-29 10:00:38 -0400

    more bed and bath options

--- home/forms.py
+++ home/forms.py
@@ -15,8 +15,8 @@ class SignUpForm(UserCreationForm):
 
 class SearchForm(forms.Form):
     search_text = forms.CharField(max_length = 30, required = True)
-    BEDS = (("1","all beds"),("2","1 bed"),("3","2 beds"),("4","3 beds"),("5","3+ beds"))
-    BATHS = (("1","all baths"),("2","1 bath"),("3","2 baths"),("4","3 baths"),("5","3+ baths"))
+    BEDS = (("1","All Beds"),("2","1 Bed"),("3","2 Beds"),("4","3 Beds"),("5","3+ Beds"))
+    BATHS = (("1","All Baths"),("2","1 Bath"),("3","2 Baths"),("4","3 Baths"),("5","3+ Baths"))
     beds = forms.ChoiceField(choices=BEDS)
     baths = forms.ChoiceField(choices=BATHS)
     days = forms.ChoiceField(choices=[(x, x) for x in range(1, 5)])
@@ -35,12 +35,13 @@ class ListingForm(ModelForm):
 
     baths_list =['1 bath','2 baths', '3 baths', '4+']
 
-    temp = [(x,x) for x in range(1,5)]
+    temp1 = [(x,x) for x in range(1,5)]
 
-    baths = forms.ChoiceField(choices=temp)
+    baths = forms.ChoiceField(choices=temp1)
 
+    temp2 = [(x,x) for x in range(1,9)]
     beds_list =['1 bed','2 beds', '3 beds', '4+']
-    beds = forms.ChoiceField(choices=temp)
+    beds = forms.ChoiceField(choices=temp2)
 
     class Meta:
         model = Listing
